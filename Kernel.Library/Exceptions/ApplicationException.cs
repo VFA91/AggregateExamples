29b5c57da74b8800ddc5b10edb45d6c580bf5e33
VFA91 <fernandez.antolin.vicente@gmail.com>
2020-02-12 14:17:07 +0100

    Add two specific Exceptions

---
+++ Kernel.Library/Exceptions/ApplicationException.cs
@@ -0,0 +1,35 @@
+﻿namespace Kernel.Library.Exceptions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// Specific exception for application.
+    /// </summary>
+    [Serializable]
+    public class ApplicationException : BaseException
+    {
+        public IList<string> ErrorMessages { get; private set; }
+
+        public ApplicationException(string errorMessage) : base(errorMessage)
+        {
+            ErrorMessages = new List<string> { errorMessage };
+        }
+
+        public ApplicationException(IList<string> errorMessages)
+            : base(string.Join("\n", errorMessages))
+        {
+            ErrorMessages = new List<string>(errorMessages);
+        }
+
+        public ApplicationException()
+        {
+        }
+
+        protected ApplicationException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
---
+++ Kernel.Library/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,27 @@
+﻿namespace Kernel.Library.Exceptions
+{
+    using System;
+    using System.Reflection;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// Specific exception for entity not found.
+    /// </summary>
+    [Serializable]
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException()
+        {
+        }
+
+        public EntityNotFoundException(MemberInfo entityType, int entityId)
+            : base($"Entity of type {entityType.Name} with id {entityId} was not found.")
+        {
+        }
+
+        protected EntityNotFoundException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
